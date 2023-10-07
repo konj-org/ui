6ec80b1b34b80c3ac35efc730ae14e38f250dc68
Amir <60965542+amir4rab@users.noreply.github.com>
2023-10-07 03:11:52 -0400

    minor bug fixes

--- src/components/preact/primitive/defaultComponents.tsx
+++ src/components/preact/primitive/defaultComponents.tsx
@@ -1,10 +1,12 @@
+/** @jsxImportSource preact */
+
 import { type ComponentProps, createElement } from "preact";
 
-export const DButton = (props: ComponentProps<"button">) =>
+export const DButton = (props: Omit<ComponentProps<"button">, "class">) =>
   createElement("button", props);
 
-export const DAnchor = (props: ComponentProps<"a">) =>
+export const DAnchor = (props: Omit<ComponentProps<"a">, "class">) =>
   createElement("a", props);
 
-export const DDiv = (props: ComponentProps<"div">) =>
+export const DDiv = (props: Omit<ComponentProps<"div">, "class">) =>
   createElement("div", props);
