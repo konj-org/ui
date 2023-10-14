3dc2b0ac29a6dea335cf465ab8a2fa7fdef69751
Amir <60965542+amir4rab@users.noreply.github.com>
2023-10-14 07:27:08 -0400

    Updates and bug fixes in button component

--- src/components/react/styled/button.tsx
+++ src/components/react/styled/button.tsx
@@ -58,312 +58,338 @@ export const buttonClassName = cva(
         false: [],
       },
       square: {
-        true: ["p-2"],
+        true: ["!px-3", "!py-3", "rounded-xl"],
         false: [],
       },
       compiledVariant: {
-        //* Theme
+        // -------------------- //
+        // --- Theme ---------- //
+        // -------------------- //
         "theme-filled": [
+          // Light Mode
           "text-neutral-950",
           "fill-neutral-950",
           "bg-neutral-300",
-          "hover:bg-neutral-200",
-          "focus-visible:bg-neutral-200",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-200",
           "active:bg-neutral-100",
+          // Dark Mode
           "dark:text-neutral-50",
           "dark:fill-neutral-50",
           "dark:bg-neutral-800",
-          "dark:hover:bg-neutral-700",
-          "dark:focus-visible:bg-neutral-700",
+          "dark:[&:is(:hover,:focus-visible)]:bg-neutral-700",
           "dark:active:bg-neutral-600",
         ],
         "theme-subtle": [
+          // Light Mode
           "text-neutral-950",
           "fill-neutral-950",
           "bg-neutral-300/50",
-          "hover:bg-neutral-200/50",
-          "focus-visible:bg-neutral-200/50",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-200/50",
           "active:bg-neutral-100/50",
+          // dark Mode
           "dark:text-neutral-50",
           "dark:fill-neutral-50",
           "dark:bg-neutral-800/50",
-          "dark:hover:bg-neutral-700/50",
-          "dark:focus-visible:bg-neutral-700/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-neutral-700/50",
           "dark:active:bg-neutral-600/50",
         ],
         "theme-ghost": [
+          // Light Mode
           "text-neutral-900",
           "fill-neutral-900",
-          "hover:text-neutral-800",
-          "hover:fill-neutral-800",
-          "focus-visible:text-neutral-800",
-          "focus-visible:fill-neutral-800",
+          "[&:is(:hover,:focus-visible)]:fill-neutral-800",
+          "[&:is(:hover,:focus-visible)]:text-neutral-800",
           "active:text-neutral-700",
           "active:fill-neutral-700",
+          // Dark Mode
           "dark:text-neutral-100",
           "dark:fill-neutral-100",
-          "dark:hover:text-neutral-200",
-          "dark:hover:fill-neutral-200",
-          "dark:focus-visible:text-neutral-200",
-          "dark:focus-visible:fill-neutral-200",
+          "dark:[&:is(:hover,:focus-visible)]:fill-neutral-200",
+          "dark:[&:is(:hover,:focus-visible)]:text-neutral-200",
           "dark:active:text-neutral-300",
           "dark:active:fill-neutral-300",
         ],
 
-        //* Light
+        // -------------------- //
+        // --- Light ---------- //
+        // -------------------- //
         "light-filled": [
           "text-neutral-950",
           "fill-neutral-950",
           "bg-neutral-300",
-          "hover:bg-neutral-200",
-          "focus-visible:bg-neutral-200",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-200",
           "active:bg-neutral-100",
         ],
         "light-subtle": [
           "text-neutral-950",
           "fill-neutral-950",
           "bg-neutral-300/50",
-          "hover:bg-neutral-200/50",
-          "focus-visible:bg-neutral-200/50",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-200/50",
           "active:bg-neutral-100/50",
         ],
         "light-ghost": [
           "text-neutral-900",
           "fill-neutral-900",
-          "hover:text-neutral-800",
-          "hover:fill-neutral-800",
-          "focus-visible:text-neutral-800",
-          "focus-visible:fill-neutral-800",
+          "[&:is(:hover,:focus-visible)]:text-neutral-800",
+          "[&:is(:hover,:focus-visible)]:fill-neutral-800",
           "active:text-neutral-700",
           "active:fill-neutral-700",
         ],
 
-        //* Dark
+        // -------------------- //
+        // --- Dark ----------- //
+        // -------------------- //
         "dark-filled": [
           "text-neutral-50",
           "fill-neutral-50",
           "bg-neutral-800",
-          "hover:bg-neutral-700",
-          "focus-visible:bg-neutral-700",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-700",
           "active:bg-neutral-600",
         ],
         "dark-subtle": [
           "text-neutral-50",
           "fill-neutral-50",
           "bg-neutral-800/50",
-          "hover:bg-neutral-700/50",
-          "focus-visible:bg-neutral-700/50",
+          "[&:is(:hover,:focus-visible)]:bg-neutral-700/50",
           "active:bg-neutral-600/50",
         ],
         "dark-ghost": [
           "text-neutral-100",
           "fill-neutral-100",
-          "hover:text-neutral-200",
-          "hover:fill-neutral-200",
-          "focus-visible:text-neutral-200",
-          "focus-visible:fill-neutral-200",
+          "[&:is(:hover,:focus-visible)]:text-neutral-200",
+          "[&:is(:hover,:focus-visible)]:text-neutral-200",
           "active:text-neutral-300",
           "active:fill-neutral-300",
         ],
 
-        //* Primary
+        // -------------------- //
+        // --- Primary -------- //
+        // -------------------- //
         "primary-filled": [
+          // Light Mode
           "text-primary-100",
           "fill-primary-100",
           "bg-primary-500",
-          "hover:bg-primary-600",
-          "focus-visible:bg-primary-600",
+          "[&:is(:hover,:focus-visible)]:bg-primary-600",
           "active:bg-primary-700",
+          // Dark Mode
           "dark:text-primary-950",
           "dark:fill-primary-950",
           "dark:bg-primary-400",
-          "dark:hover:bg-primary-500",
-          "dark:focus-visible:bg-primary-500",
+          "dark:[&:is(:hover,:focus-visible)]:bg-primary-500",
           "dark:active:bg-primary-600",
         ],
         "primary-subtle": [
+          // Light Mode
           "text-primary-950",
           "fill-primary-950",
           "bg-primary-500/50",
-          "hover:bg-primary-600/50",
-          "focus-visible:bg-primary-600/50",
+          "[&:is(:hover,:focus-visible)]:bg-primary-600/50",
           "active:bg-primary-700/50",
+          // Dark Mode
           "dark:text-primary-100",
           "dark:fill-primary-100",
           "dark:bg-primary-400/50",
-          "dark:hover:bg-primary-500/50",
-          "dark:focus-visible:bg-primary-500/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-primary-500/50",
           "dark:active:bg-primary-600/50",
         ],
         "primary-ghost": [
+          // Light Mode
           "text-primary-600",
-          "hover:text-primary-700",
-          "focus-visible:text-primary-700",
+          "fill-primary-600",
+          "[&:is(:hover,:focus-visible)]:text-primary-500",
           "active:text-primary-800",
+          "active:fill-primary-800",
+          // Dark Mode
           "dark:text-primary-400",
-          "dark:hover:text-primary-500",
-          "dark:focus-visible:text-primary-500",
+          "dark:fill-primary-400",
+          "dark:[&:is(:hover,:focus-visible)]:text-primary-500",
           "dark:active:text-primary-600",
+          "dark:active:fill-primary-600",
         ],
 
-        //* Danger
+        // -------------------- //
+        // --- Danger --------- //
+        // -------------------- //
         "danger-filled": [
+          // Light Mode
           "text-red-100",
           "fill-red-100",
           "bg-red-500",
-          "hover:bg-red-600",
-          "focus-visible:bg-red-600",
+          "[&:is(:hover,:focus-visible)]:bg-red-600",
           "active:bg-red-700",
+          // Dark Mode
           "dark:text-red-950",
           "dark:fill-red-950",
           "dark:bg-red-400",
-          "dark:hover:bg-red-500",
-          "dark:focus-visible:bg-red-500",
+          "dark:[&:is(:hover,:focus-visible)]:bg-red-500",
           "dark:active:bg-red-600",
         ],
         "danger-subtle": [
+          // Light Mode
           "text-red-950",
           "fill-red-950",
           "bg-red-500/50",
-          "hover:bg-red-600/50",
-          "focus-visible:bg-red-600/50",
+          "[&:is(:hover,:focus-visible)]:bg-red-600/50",
           "active:bg-red-700/50",
+          // Dark Mode
           "dark:text-red-100",
           "dark:fill-red-100",
           "dark:bg-red-400/50",
-          "dark:hover:bg-red-500/50",
-          "dark:focus-visible:bg-red-500/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-red-500/50",
           "dark:active:bg-red-600/50",
         ],
         "danger-ghost": [
+          // Light Mode
           "text-red-600",
-          "hover:text-red-700",
-          "focus-visible:text-red-700",
+          "fill-red-600",
+          "[&:is(:hover,:focus-visible)]:text-red-500",
           "active:text-red-800",
+          "active:fill-red-800",
+          // Dark Mode
           "dark:text-red-400",
-          "dark:hover:text-red-500",
-          "dark:focus-visible:text-red-500",
+          "dark:fill-red-400",
+          "dark:[&:is(:hover,:focus-visible)]:text-red-500",
           "dark:active:text-red-600",
+          "dark:active:fill-red-600",
         ],
 
-        //* Link
+        // -------------------- //
+        // --- Link ----------- //
+        // -------------------- //
         "link-filled": [
+          // Light Mode
           "text-blue-100",
           "fill-blue-100",
           "bg-blue-500",
-          "hover:bg-blue-600",
-          "focus-visible:bg-blue-600",
+          "[&:is(:hover,:focus-visible)]:bg-blue-600",
           "active:bg-blue-700",
+          // Dark Mode
           "dark:text-blue-950",
           "dark:fill-blue-950",
           "dark:bg-blue-400",
-          "dark:hover:bg-blue-500",
-          "dark:focus-visible:bg-blue-500",
+          "dark:[&:is(:hover,:focus-visible)]:bg-blue-500",
           "dark:active:bg-blue-600",
         ],
         "link-subtle": [
+          // Light Mode
           "text-blue-950",
           "fill-blue-950",
           "bg-blue-500/50",
-          "hover:bg-blue-600/50",
-          "focus-visible:bg-blue-600/50",
+          "[&:is(:hover,:focus-visible)]:bg-blue-600/50",
           "active:bg-blue-700/50",
+          // Dark Mode
           "dark:text-blue-100",
           "dark:fill-blue-100",
           "dark:bg-blue-400/50",
-          "dark:hover:bg-blue-500/50",
-          "dark:focus-visible:bg-blue-500/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-blue-500/50",
           "dark:active:bg-blue-600/50",
         ],
         "link-ghost": [
+          // Light Mode
           "text-blue-600",
-          "hover:text-blue-700",
-          "focus-visible:text-blue-700",
+          "fill-blue-600",
+          "[&:is(:hover,:focus-visible)]:text-blue-500",
           "active:text-blue-800",
+          "active:fill-blue-800",
+          // Dark Mode
           "dark:text-blue-400",
-          "dark:hover:text-blue-500",
-          "dark:focus-visible:text-blue-500",
+          "dark:fill-blue-400",
+          "dark:[&:is(:hover,:focus-visible)]:text-blue-500",
           "dark:active:text-blue-600",
+          "dark:active:fill-blue-600",
         ],
 
-        //* Successful
+        // -------------------- //
+        // --- Successful ----- //
+        // -------------------- //
         "successful-filled": [
+          // Light Mode
           "text-green-100",
           "fill-green-100",
           "bg-green-500",
-          "hover:bg-green-600",
-          "focus-visible:bg-green-600",
+          "[&:is(:hover,:focus-visible)]:bg-green-600",
           "active:bg-green-700",
+          // Dark Mode
           "dark:text-green-950",
           "dark:fill-green-950",
           "dark:bg-green-400",
-          "dark:hover:bg-green-500",
-          "dark:focus-visible:bg-green-500",
+          "dark:[&:is(:hover,:focus-visible)]:bg-green-500",
           "dark:active:bg-green-600",
         ],
         "successful-subtle": [
+          // Light Mode
           "text-green-950",
           "fill-green-950",
           "bg-green-500/50",
-          "hover:bg-green-600/50",
-          "focus-visible:bg-green-600/50",
+          "[&:is(:hover,:focus-visible)]:bg-green-600/50",
           "active:bg-green-700/50",
+          // Dark Mode
           "dark:text-green-100",
           "dark:fill-green-100",
           "dark:bg-green-400/50",
-          "dark:hover:bg-green-500/50",
-          "dark:focus-visible:bg-green-500/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-green-500/50",
           "dark:active:bg-green-600/50",
         ],
         "successful-ghost": [
+          // Light Mode
           "text-green-600",
-          "hover:text-green-700",
-          "focus-visible:text-green-700",
+          "fill-green-600",
+          "[&:is(:hover,:focus-visible)]:text-green-500",
           "active:text-green-800",
+          "active:fill-green-800",
+          // Dark Mode
           "dark:text-green-400",
-          "dark:hover:text-green-500",
-          "dark:focus-visible:text-green-500",
+          "dark:fill-green-400",
+          "dark:[&:is(:hover,:focus-visible)]:text-green-500",
           "dark:active:text-green-600",
+          "dark:active:fill-green-600",
         ],
 
-        //* Warning
+        // -------------------- //
+        // --- Warning -------- //
+        // -------------------- //
         "warning-filled": [
+          // Light Mode
           "text-yellow-100",
           "fill-yellow-100",
           "bg-yellow-500",
-          "hover:bg-yellow-600",
-          "focus-visible:bg-yellow-600",
+          "[&:is(:hover,:focus-visible)]:bg-yellow-600",
           "active:bg-yellow-700",
+          // Dark Mode
           "dark:text-yellow-950",
           "dark:fill-yellow-950",
           "dark:bg-yellow-400",
-          "dark:hover:bg-yellow-500",
-          "dark:focus-visible:bg-yellow-500",
+          "dark:[&:is(:hover,:focus-visible)]:bg-yellow-500",
           "dark:active:bg-yellow-600",
         ],
         "warning-subtle": [
+          // Light Mode
           "text-yellow-950",
           "fill-yellow-950",
           "bg-yellow-500/50",
-          "hover:bg-yellow-600/50",
-          "focus-visible:bg-yellow-600/50",
+          "[&:is(:hover,:focus-visible)]:bg-yellow-600/50",
           "active:bg-yellow-700/50",
+          // Dark Mode
           "dark:text-yellow-100",
           "dark:fill-yellow-100",
           "dark:bg-yellow-400/50",
-          "dark:hover:bg-yellow-500/50",
-          "dark:focus-visible:bg-yellow-500/50",
+          "dark:[&:is(:hover,:focus-visible)]:bg-yellow-500/50",
           "dark:active:bg-yellow-600/50",
         ],
         "warning-ghost": [
-          "text-yellow-500",
-          "hover:text-yellow-600",
-          "focus-visible:text-yellow-600",
-          "active:text-yellow-700",
+          // Light Mode
+          "text-yellow-600",
+          "fill-yellow-600",
+          "[&:is(:hover,:focus-visible)]:text-yellow-500",
+          "active:text-yellow-800",
+          "active:fill-yellow-800",
+          // Dark Mode
           "dark:text-yellow-400",
-          "dark:hover:text-yellow-500",
-          "dark:focus-visible:text-yellow-500",
+          "dark:fill-yellow-400",
+          "dark:[&:is(:hover,:focus-visible)]:text-yellow-500",
           "dark:active:text-yellow-600",
+          "dark:active:fill-yellow-600",
         ],
       },
       color: {
