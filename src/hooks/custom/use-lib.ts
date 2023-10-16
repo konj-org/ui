c7fd8619288a78e740de35903fec4639254f429d
Amir <60965542+amir4rab@users.noreply.github.com>
2023-10-16 07:04:27 -0400

    added persistence to use-lib

--- src/hooks/custom/use-lib.ts
+++ src/hooks/custom/use-lib.ts
@@ -1,5 +1,31 @@
+import type { UILibraries } from "@/data/types";
 import { signal } from "@preact/signals";
+import { useCallback, useEffect } from "preact/hooks";
 
-export const librarySignal = signal<"react" | "preact">("react");
+export const librarySignal = signal<UILibraries>("react");
 
-export const useLibrary = () => librarySignal;
+const localStorageKey = "KONJ_UI_PREFERRED_LIB";
+const possibleLibraries: UILibraries[] = ["preact", "react", "solid"];
+
+export const useLibrary: () => [UILibraries, (v: UILibraries) => void] = () => {
+  const setLibrary = useCallback((v: UILibraries) => {
+    localStorage.setItem(localStorageKey, v);
+    librarySignal.value = v;
+  }, []);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    const v = localStorage.getItem(localStorageKey);
+    console.log(v);
+
+    if (
+      v !== null &&
+      v !== librarySignal.peek() &&
+      possibleLibraries.includes(v as UILibraries)
+    ) {
+      librarySignal.value = v as UILibraries;
+    }
+  }, []);
+
+  return [librarySignal.value, setLibrary];
+};
