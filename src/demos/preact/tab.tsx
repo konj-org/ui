55fe11547946ec1d4f58a7d90a0054aa086a7dac
Amir <60965542+amir4rab@users.noreply.github.com>
2023-10-26 09:09:26 -0400

    minor visual fixes

--- src/demos/preact/tab.tsx
+++ src/demos/preact/tab.tsx
@@ -4,7 +4,7 @@ import { Tab, TabItem } from "@/components/preact/styled/tab";
 
 export const TabDemo = () => {
   return (
-    <Tab className="min-h-[40vh] flex flex-col justify-center align-middle items-stretch content-stretch">
+    <Tab className="flex flex-col justify-center align-middle items-stretch content-stretch">
       <TabItem id="t-1" title="Preview">
         <p className="text-center">Preview of the component</p>
       </TabItem>
